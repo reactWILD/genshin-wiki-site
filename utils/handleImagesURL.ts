75e58bc3b248805ad17f384435bec7c9272b67a9
Francisco5z <58224350+reactWILD@users.noreply.github.com>
2021-12-24 09:54:29 -0300

    Update api images URL handler

--- utils/handleImagesURL.ts
+++ utils/handleImagesURL.ts
@@ -1,3 +1,8 @@
+const apiUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3333"
+    : process.env.NEXT_PUBLIC_API_URL
+
 export default function handleImagesURL(path: string) {
-  return `http://localhost:3333${path}`
+  return ${apiUrl}${path}`
 }
